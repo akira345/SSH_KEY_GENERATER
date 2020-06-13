8ea82c133d26d1028078ba4225c8e65ba57679e9
Akira345 <g.akira345@gmail.com>
2020-06-14 00:14:56 +0900

    64bitにする

--- SSH_KEY_GENERATER/Properties/AssemblyInfo.cs
+++ SSH_KEY_GENERATER/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SSH_KEY_GENERATER")]
-[assembly: AssemblyCopyright("Copyright ©  2018 Akira345")]
+[assembly: AssemblyCopyright("Copyright ©  2020 Akira345")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.9.0")]
+[assembly: AssemblyFileVersion("2.0.9.0")]
